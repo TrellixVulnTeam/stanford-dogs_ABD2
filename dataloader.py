64096d8d938bce1ecc45f4c1495d9933fc6f197a
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 04:02:37 +0000

    Adding numeric_owner as keyword arguement

--- dataloader.py
+++ dataloader.py
@@ -90,7 +90,7 @@ class StanfordDogsDataset(Dataset):
                         if not is_within_directory(path, member_path):
                             raise Exception("Attempted Path Traversal in Tar File")
                 
-                    tar.extractall(path, members, numeric_owner) 
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
                     
                 
                 safe_extract(f, downloads_dir)
